47a1415b454cdd1b3368bc836d31bafd7627700c
Nathan Rajlich <nathan@tootallnate.net>
2013-08-04 14:01:18 -0700

    test: refactor mocha tests, add `Connection: close` test

--- test/test.js
+++ test/test.js
@@ -9,23 +9,41 @@ var assert = require('assert');
 var Agent = require('../');
 
 describe('Agent', function () {
+  var server;
 
-  it('should work in simple http requests', function (done) {
-    // set up a local HTTP server
-    var server = http.createServer(function (req, res) {
+  // setup test HTTP server
+  before(function (done) {
+    server = http.createServer(function (req, res) {
       res.setHeader('X-It-Works', 'yesss');
       res.end();
     });
-    server.listen(0, function () {
-      var port = server.address().port;
-      var info = url.parse('http://127.0.0.1:' + port + '/foo');
-      info.agent = new Agent();
-      http.get(info, function (res) {
-        assert.equal('yesss', res.headers['x-it-works']);
-        server.on('close', done);
-        server.close();
-      });
+    server.listen(0, done);
+  });
+
+  it('should work for basic HTTP requests', function (done) {
+    var port = server.address().port;
+    var info = url.parse('http://127.0.0.1:' + port + '/foo');
+    info.agent = new Agent();
+    http.get(info, function (res) {
+      assert.equal('yesss', res.headers['x-it-works']);
+      done();
     });
+  });
+
+  it('should set the `Connection: close` response header', function (done) {
+    var port = server.address().port;
+    var info = url.parse('http://127.0.0.1:' + port + '/bar');
+    info.agent = new Agent();
+    http.get(info, function (res) {
+      assert.equal('close', res.headers.connection);
+      done();
+    });
+  });
+
+  // shut down test HTTP server
+  after(function (done) {
+    server.once('close', done);
+    server.close();
   });
 
 });
