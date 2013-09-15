ad82a1df79be260f97eb8b4c997c0da1fac58dfe
Nathan Rajlich <nathan@tootallnate.net>
2013-09-14 18:56:33 -0700

    test: add an artificial HTTP GET request test

--- test/test.js
+++ test/test.js
@@ -42,8 +42,31 @@ describe('Agent', function () {
       });
     });
   });
-  describe('artificial "data" events', function () {
-    it('should work for an artificial EventEmitter "stream" GET request', function (done) {
+  describe('artificial "streams"', function () {
+    it('should send a GET request', function (done) {
+      var stream = new events.EventEmitter();
+
+      // needed for the `http` module to call .write() on the stream
+      stream.writable = true;
+
+      stream.write = function (str) {
+        assert(0 == str.indexOf('GET / HTTP/1.1'));
+        done();
+      };
+
+      var opts = {
+        method: 'GET',
+        host: '127.0.0.1',
+        path: '/',
+        port: 80,
+        agent: new Agent(function (req, opts, fn) {
+          fn(null, stream);
+        })
+      };
+      var req = http.request(opts);
+      req.end();
+    });
+    it('should receive a GET response', function (done) {
       var stream = new events.EventEmitter();
       var opts = {
         method: 'GET',
